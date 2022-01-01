fe6135d05d6dc2de352a469e09043efd8d816fac
Gleb <glebars2004@gmail.com>
2022-01-01 18:09:25 +0200

    fixed server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ server.use(middlewares);
 server.use(jsonServer.rewriter({ '/api/*': '/$1', })) 
 server.use(router);
 
-server.listen(port, () => { console.log('Server is running'));
+server.listen(port, () => { console.log('Server is running')});
 
