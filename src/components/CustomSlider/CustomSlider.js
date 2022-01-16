a9783d5002887f09679ad9c019ad8a4c4f1d8814
Gleb <glebars2004@gmail.com>
2022-01-16 03:35:39 +0200

    customized dots, arrows, other elements in slider

--- src/components/CustomSlider/CustomSlider.js
+++ src/components/CustomSlider/CustomSlider.js
@@ -7,15 +7,63 @@ import purpleLine from '../../img/purple-line.svg'
 import appleLogo from '../../img/apple-icon.png'
 import appleLogoo from '../../img/apple-icon24.png'
 
+function SampleNextArrow(props) {
+  const {onClick } = props;
+  return (
+    <div
+      className="slider__arrow"
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const {onClick } = props;
+  return (
+    <div
+      className="slider__arrow slider__arrow_left"
+      onClick={onClick}
+    />
+  );
+}
+
+
 const settings = {
   dots: true,
   infinite: true,
-  speed: 500,
+  speed: 750,
   slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
+  appendDots: dots => (
+    <div style={{bottom: 0}} >
+      <ul className="slider__dots-list"> {dots} </ul>
+    </div>
+  ),
+  customPaging: index => (
+    <div className="slider__dot"></div>
+  )
+
 };
 
 const sliderData = [
+  {
+    title: "iPhone 12",
+    text: "Теперь в фиолетовом",
+    price: "от 23 897",
+    btnText: "Узнать больше",
+    productImg: purplePhone,
+    lineImg: purpleLine,
+  },
+  {
+    title: "iPhone 12",
+    text: "Теперь в фиолетовом",
+    price: "от 23 897",
+    btnText: "Узнать больше",
+    productImg: purplePhone,
+    lineImg: purpleLine,
+  },
   {
     title: "iPhone 12",
     text: "Теперь в фиолетовом",
@@ -39,17 +87,18 @@ const CustomSlider = () => {
       <div className="slider">
         <Slider {...settings}>
           {sliderData.map(slide => 
-            <div key={slide.title} className="slider__slide">
-              <img src={slide.lineImg} alt="" />
-              <div className="slider__slide__text-container">
-                <img src={appleLogo} alt="" />
-                <img src={appleLogoo} alt="" />
-                <h4 className="slider__slide__title">{slide.title}</h4>
-                <h4 className="slider__slide__text">{slide.text}</h4>
-                <h5 className="slider__slide__price">{slide.price}</h5>
-                <button className="slider__slide__button">{slide.btnText}</button>
+            <div key={slide.title}>
+              <div style={{backgroundImage: `url(${slide.lineImg})`}} className="slider__slide">
+                <div className="slider__slide__text-container">
+                  <img src={appleLogo} alt="" />
+                  <img src={appleLogoo} alt="" />
+                  <h4 className="slider__slide__title">{slide.title}</h4>
+                  <h4 className="slider__slide__text">{slide.text}</h4>
+                  <h5 className="slider__slide__price">{slide.price}</h5>
+                  <button className="slider__slide__button">{slide.btnText}</button>
+                </div>
+                <img src={slide.productImg} alt="img" className="slider__slide__img"/>
               </div>
-              <img src={slide.productImg} alt="img" className="slider__slide__img"/>
             </div>
           )}
         </Slider>
