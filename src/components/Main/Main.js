0de45478fddc7b463e142d1aea83255af7b23c54
Gleb <glebars2004@gmail.com>
2022-01-17 02:08:24 +0200

    implemented banners on main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import OrderForm from "../OrderForm/OrderForm";
 import ProductsList from '../ProductsList/ProductsList'
 import { useSelector } from "react-redux";
 import CustomSlider from '../CustomSlider/CustomSlider'
+import watchImg from "../../img/watch-banner.png"
+import phoneImg from "../../img/phone-banner.png"
+import macImg from "../../img/mac-banner.png"
+import tvImg from "../../img/tv-banner.png"
+import podsImg from "../../img/pods-banner.png"
+
 
 const Main = () => {
 
@@ -19,8 +25,34 @@ const Main = () => {
   }, []);
 
   return (
+    <div className="main">
+      <CustomSlider/>
+      <div className="main__banners-container">
+        <Link to="favorites">
+          <div className="main__banner">
+            <img src={watchImg} alt="" className="main__banner__img"/>
+            <span className="main__banner__title">Watch</span>
+          </div>
+        </Link>
+        <div className="main__banner">
+          <img src={phoneImg} alt="" className="main__banner__img"/>
+          <span className="main__banner__title">iPhone</span>
+        </div>
+        <div className="main__banner">
+          <img src={macImg} alt="" className="main__banner__img"/>
+          <span className="main__banner__title">Mac</span>
+        </div>
+        <div className="main__banner">
+          <img src={tvImg} alt="" className="main__banner__img"/>
+          <span className="main__banner__title">TV</span>
+        </div>
+        <div className="main__banner">
+          <img src={podsImg} alt="" className="main__banner__img"/>
+          <span className="main__banner__title">AirPods</span>
+        </div>
+      </div>
+    </div>
 
-    <CustomSlider/>
 
 
     // <OrderForm/>
