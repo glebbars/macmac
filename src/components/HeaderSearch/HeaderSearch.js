5790d24a85181d018b3417332d066e398c4f67c9
Gleb <glebars2004@gmail.com>
2022-02-08 02:43:59 +0200

    modifyed header search + built a fundament for a search suggestions

--- src/components/HeaderSearch/HeaderSearch.js
+++ src/components/HeaderSearch/HeaderSearch.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import Search from "../Search/Search";
 
 const HeaderSearch = () => {
 
-  return(
-    <Search className='header__search' placeholder='Категория, название товара, артикул'/>
+  const [openedSearchWrapper, setOpenedSearchWrapper] = useState(false)
+  const [touchedSearch, setTouchedSearch] = useState(false)
 
+  return(
+    <div onClick={() => {
+      setTouchedSearch(true); 
+      if(openedSearchWrapper) {
+        setOpenedSearchWrapper(false);
+        setTouchedSearch(false)
+      } 
+    }}
+    className={`header__search-screen-wrapper ${openedSearchWrapper ? 'header__search-screen-wrapper_active' : ''} ${touchedSearch ? 'header__search-screen-wrapper_mobile' : ''} `}>
+    <div className={`header__search-container ${openedSearchWrapper ? 'header__search-container_active' : ''} ${touchedSearch ? 'header__search-container_mobile' : ''}`}>
+      <Search onType={(text) => text.length > 0 ? setOpenedSearchWrapper(true) : null} 
+      className={`header__search ${openedSearchWrapper ? 'header__search_active' : ''} ${touchedSearch ? 'header__search_mobile' : ''}`} placeholder={openedSearchWrapper ? 'Поиск по товарам' : 'Категория, название товара, артикул'}/>
+    </div>
+    </div>
   )
 }
 
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import search from "../../img/search.svg"
 
-const Search = ({className, placeholder}) =>{
+const Search = ({className, placeholder, onType}) =>{
 
   return(
       <div className={className}>
         <img src={search} alt="search" />
-        <input type="search" className='header__search__input' placeholder={placeholder}/>
+        <input onChange={(e) => onType(e.target.value)} type="search" className='header__search__input' placeholder={placeholder}/>
     </div>
   )
 }
