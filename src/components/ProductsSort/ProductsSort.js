b616758bbb893991baf83a5eadd6278d68915419
Gleb <glebars2004@gmail.com>
2022-02-22 09:05:51 +0200

    testing notifications

--- src/components/ProductsSort/ProductsSort.js
+++ src/components/ProductsSort/ProductsSort.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 const ProductsSort = () => {
   const [sortName, setSortName] = useState('Убыванию цены')
 
+  
   return(
     <div className='products__sort'>
       <span className='products__sort__header'>Сортировать по:</span>
