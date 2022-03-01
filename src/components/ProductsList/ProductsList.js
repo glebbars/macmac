49147f52db51946eac4d78c832cea2c4242dd002
Gleb <glebars2004@gmail.com>
2022-03-02 01:48:55 +0200

    implemented initial filtering: by categories

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -7,20 +7,27 @@ import { useSelector } from "react-redux";
 
 const ProductsList = () => {
   const [currentPage, setCurrentPage] = useState(1);
-
   const productsArr = useSelector((store) => store.app.productsArr);
   const productsListFilters = useSelector((store) => store.app.productsListFilters);
   const sortType = useSelector(store => store.app.sortType)
-
   
   useEffect(() => {
     setCurrentPage(1)
     console.log(productsListFilters)
   }, [productsListFilters])
+
+  const filteredByCategoryArr = productsArr.filter(product => {
+    const currentPathName = window.location.pathname
+    if(currentPathName.includes('/category/') && !currentPathName.includes('/all-products')){
+      return product.category === currentPathName.split('/category/')[1]
+    } else{
+      return product
+    }
+  })
   
-  const filteredProductsArr = productsArr.filter(product => {
-    const fullProductName = `${product.category} ${product.model} ${product.capacity} ${product.color}`
+  const filteredProductsArr = filteredByCategoryArr.filter(product => {
     if(productsListFilters.length > 0){
+      const fullProductName = `${product.category} ${product.model} ${product.capacity} ${product.color}`
       return productsListFilters.some(filter => fullProductName.includes(filter.text.toLowerCase()))
     } else{
       return product
@@ -37,10 +44,8 @@ const ProductsList = () => {
   })
 
   const currentTableData = useMemo(() => {
-    
     const firstPageIndex = (currentPage - 1) * 34;
     const lastPageIndex = firstPageIndex + 34;
-    
     return sortedProductsArr.slice(firstPageIndex, lastPageIndex);
   }, [currentPage, sortedProductsArr]);
   
@@ -51,7 +56,7 @@ const ProductsList = () => {
         className="pagination-bar"
         currentPage={currentPage}
         totalCount={sortedProductsArr.length}
-        pageSize={4}
+        pageSize={34}
         onPageChange={page => setCurrentPage(page)}
       />
     {currentTableData && <List productsArr={currentTableData}/> }
