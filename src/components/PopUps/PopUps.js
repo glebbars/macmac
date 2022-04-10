20ccedf6fb6f95c9fdd9f721a4ea07fcc088a534
Gleb <glebars2004@gmail.com>
2022-04-10 14:41:47 +0300

    purchase pop up

--- src/components/PopUps/PopUps.js
+++ src/components/PopUps/PopUps.js
@@ -2,6 +2,9 @@ import React from 'react'
 import OrderForm from '../OrderForm/OrderForm'
 import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
 import "react-popupbox/dist/react-popupbox.css"
+import bag from '../../img/shopping-bag-black.svg'
+import bagGrey from '../../img/shopping-bag.svg'
+import {Link} from 'react-router-dom'
 
 export const openPopupboxOneClick = (productDataObj) => {
   const fullProductName = `${productDataObj.category} ${productDataObj.model} ${productDataObj.capacity} ${productDataObj.color}`
@@ -29,11 +32,39 @@ export const openPopupboxOneClick = (productDataObj) => {
   })
 };
 
-export const parchasePopUp = () => {
+export const PurchasePopUp = ({productDataObj, closePopUp}) => {
 
   return (
-    <div>
-      <div></div>
+    <div className='pop-up__purchase'>
+      <div onClick={closePopUp}  className='pop-up__purchase__close-wrapper'></div>
+      <div className='pop-up__purchase__wrapper'>
+        <div onClick={closePopUp} className='pop-up__purchase__cross'></div>
+        <header className='pop-up__purchase__header'>
+          <img src={bag} alt="" className='pop-up__purchase__header__img' />
+          <h1 className='pop-up__purchase__header__text'>Корзина</h1>
+          <span className='pop-up__purchase__header__length'></span>
+        </header>
+        <main className='pop-up__purchase__main'>
+          <span>Название товара</span>
+          <span>Цена</span>
+          <img src={productDataObj.pictures[0]} alt="" />
+          <div>
+            <h2>Apple iPhone X 128Gb Grey</h2>
+            <span>Емкость: 64Gb</span>
+            <span>Цвет: Space Grey</span>
+          </div>
+          <h3>{productDataObj.price}</h3>
+          <div className='pop-up__purchase__cross pop-up__purchase__cross_product'></div>
+        </main>
+        <div className='pop-up__purchase__line'></div>
+        <div>
+          <Link to='bag'>
+            <img src={bagGrey} alt="" />
+            Корзина
+          </Link>
+          <Link to='order'>Оформить заказ</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ProductMainPurchase/ProductMainPurchase.js
+++ src/components/ProductMainPurchase/ProductMainPurchase.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bagIcon from '../../img/product-bag.svg'
-import {openPopupboxOneClick} from '../PopUps/PopUps'
+import {openPopupboxOneClick, PurchasePopUp} from '../PopUps/PopUps'
 
 const ProductMainPurchase = ({productDataObj}) => {
+  const [openedPerchasePopUp, setOpenedPurchasePopUp] = useState(false)
 
-  return(
+  const closePopUp = () => setOpenedPurchasePopUp(false)
+
+  return (
     <div className='product__main__purchase'>
-      <div className='product__main__purchase__buy'>
+      <div onClick={() => setOpenedPurchasePopUp(true)} className='product__main__purchase__buy'>
         <img className='product__main__purchase__buy__img' src={bagIcon} alt="" />
         <span className='product__main__purchase__buy__text'>Купить</span>
       </div>
       <div onClick={() => openPopupboxOneClick(productDataObj)} className='product__main__purchase__one-click'>
         <span className='product__main__purchase__one-click__text'>Купить в один клик</span>
       </div>
+      {openedPerchasePopUp && <PurchasePopUp closePopUp={closePopUp}  productDataObj={productDataObj} />}
     </div>
   )
 }
