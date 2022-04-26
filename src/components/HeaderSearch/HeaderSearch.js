3883daff94872f1e7294beae4db0a7e3f3349423
Gleb <glebars2004@gmail.com>
2022-04-26 21:21:42 +0300

    modifyed search engine

--- src/components/HeaderSearch/HeaderSearch.js
+++ src/components/HeaderSearch/HeaderSearch.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Search from "../Search/Search";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Highlighter from "react-highlight-words";
+
 
 const HeaderSearch = () => {
   const productsArr = useSelector((store) => store.app.productsArr)
+  const [searchText, setSearchText] = useState('')
   const [openedSearchWrapper, setOpenedSearchWrapper] = useState(false)
   const [touchedSearch, setTouchedSearch] = useState(false)
   const [filteredArr, setFilteredArr] = useState([])
+  const location = useLocation()
   const history = useHistory()
 
+  useEffect(() => {
+    handleToggle()
+  }, [location])
+
   const handleSubmit = (event, ref) => {
+    if(filteredArr.length === 1){
+      const product = filteredArr[0]
+
+      history.push(`/category/${product.category.toLowerCase()}/${product.id}`)
+    } else{
+      history.push(`/search/${ref.current.value}`)
+    }
+
     event.preventDefault()
     setOpenedSearchWrapper(false)
     setTouchedSearch(false)
-    history.push(`/search/${ref.current.value}`)
     ref.current.blur()
     ref.current.value = ""
   }
 
   const habndleType = (text) => {
+    setSearchText(text)
+    
     if(text.length > 0 && !openedSearchWrapper) {
       setOpenedSearchWrapper(true)
     }
@@ -35,15 +52,28 @@ const HeaderSearch = () => {
     return setFilteredArr(filteredArr)
   }
 
+  const handleToggle = () => {
+
+    if(openedSearchWrapper){
+      setOpenedSearchWrapper(false)
+    }
+
+    if(touchedSearch){
+      setTouchedSearch(false)
+    }
+  }
+
+  const categoriesArr = filteredArr.map(product => product.category)
+  const categorySet = new Set(categoriesArr)
+  const categoriesSetArr = Array.from(categorySet)
+
+
   return(
     <>
-    <div onClick={() => {
-      setTouchedSearch(false); 
-      if(openedSearchWrapper) {
-        setOpenedSearchWrapper(false);
-      } 
-    }}
-    className={`header__search-screen-wrapper ${openedSearchWrapper ? 'header__search-screen-wrapper_searching' : ''} ${touchedSearch ? 'header__search-screen-wrapper_touched' : ''} `}></div>
+    <div 
+      onClick={handleToggle}
+      className={`header__search-screen-wrapper ${openedSearchWrapper ? 'header__search-screen-wrapper_searching' : ''} ${touchedSearch ? 'header__search-screen-wrapper_touched' : ''} `}>
+    </div>
     <div 
       onClick={() => setTouchedSearch(true)} 
       className={`header__search-container ${touchedSearch ? 'header__search-container_touched' : ''} ${openedSearchWrapper ? 'header__search-container_searching' : ''}`}>
@@ -57,22 +87,49 @@ const HeaderSearch = () => {
 
       {filteredArr.length > 0 && (
         <div className={`header__search__result ${openedSearchWrapper ? 'header__search__result_active' : ''}`}>
-          <h3 className="header__search__result__header">Товары ({filteredArr.length} резульатов)</h3>
-          {filteredArr.map((product, index) => (
-            (index < 4) && (
-              <div className="header__search__result__product">
-                <div className="header__search__result__product__img-wrapper">
-                  <img src={product.pictures[0].url} alt="" className="header__search__result__product__img"/>
-                </div>
-                <div className="header__search__result__product__text-wrapper">  
-                  <p className="header__search__result__product__text">{product.brand} {product.category} {product.model} {product.color} {product.capacity}</p>
-                  <span className="header__search__result__product__price">{product.price.toLocaleString()}.00&#x20b4;</span>
-                </div>
-              </div>
-              )
-          ))}
 
-          <Link className="header__search__result__review-all-link" to='/search/apple'>Посмотреть все результаты</Link>
+          {categoriesSetArr.length > 0 && (
+            <div className="header__search__result__categories">
+              <h3 className="header__search__result__header">Категории</h3>
+              {categoriesSetArr.map((category, index) => (
+                <Link key={index} className="header__search__result__categories__link" to={`/search/${category.toLowerCase()}`}>
+                  <Highlighter
+                    className="header__search__result__categories__text"
+                    highlightClassName="header__search__result__categories__text__highlighted"
+                    searchWords={[searchText]}
+                    autoEscape={true}
+                    textToHighlight={category}
+                  />
+                </Link>
+              ))}
+            </div>
+          )}
+
+          <div className="header__search__result__products">
+            <h3 className="header__search__result__header">Товары ({filteredArr.length} резульатов)</h3>
+            {filteredArr.map((product, index) => (
+              (index < 4) && (
+                <div key={index} className="header__search__result__product">
+                  <div className="header__search__result__product__img-wrapper">
+                    <img src={product.pictures[0].url} alt="" className="header__search__result__product__img"/>
+                  </div>
+                  <Link to={`/category/${product.category.toLowerCase()}/${product.id}`} className="header__search__result__product__text-wrapper">  
+                    {/* <p className="header__search__result__product__text">{product.brand} {product.category} {product.model} {product.color} {product.capacity}</p> */}
+                    <Highlighter
+                      className="header__search__result__product__text"
+                      highlightClassName="header__search__result__product__highlighted"
+                      searchWords={[searchText]}
+                      autoEscape={true}
+                      textToHighlight={`${product.brand} ${product.category} ${product.model} ${product.capacity} ${product.color}`}
+                    />
+                    <span className="header__search__result__product__price">{product.price.toLocaleString()}.00&#x20b4;</span>
+                  </Link>
+                </div>
+                )
+            ))}
+          </div>
+
+          <Link to={`/search/${searchText}`} className="header__search__result__review-all-link">Посмотреть все результаты</Link>
         </div>
       )}
 
