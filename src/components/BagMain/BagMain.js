b4c4b82c89c02b8cdcb6f33d80cfced0f1a194e7
Gleb <glebars2004@gmail.com>
2022-05-19 00:07:24 +0100

    cleaned code

--- src/components/BagMain/BagMain.js
+++ src/components/BagMain/BagMain.js
@@ -6,15 +6,6 @@ import { useDispatch, useSelector } from 'react-redux';
 const BagMain = ({addedToBag, filteredArr}) => {
   const dispatch = useDispatch()
   const [finalProductsArr, setFinalOrder] = useState([])
-  // const [totalOrderPrice, setTotalOrderPrice] = useState(0)
-
-  // useEffect(() => {
-  //   if(filteredArr.length > 0){
-  //     const prices = filteredArr.map(product => product.price)
-  //     const total = prices.reduce((a, c) => a + c)
-  //     setTotalOrderPrice(total)
-  //   }
-  // }, [])
 
   useEffect(() => {
     const sortedArr = filteredArr.sort((a, b) => {
@@ -24,23 +15,20 @@ const BagMain = ({addedToBag, filteredArr}) => {
     sortedArr.forEach(product => product.quantity = 1)
 
     setFinalOrder(sortedArr)
-  }, [])
+  }, [addedToBag])
 
 
   const removeFromTheBag = (productId, price) => {
+    console.log('****')
     dispatch({
       type: 'REMOVE_FROM_BAG',
       payload: addedToBag.filter((id) => id !== productId),
     })
-
-    // setTotalOrderPrice(totalOrderPrice - price)
   };
 
 
   return(
     <div className="bag__main__wrapper">
-      {/* <BagList sortedArr={sortedArr} removeFromTheBag={removeFromTheBag} totalOrderPrice={totalOrderPrice} setTotalOrderPrice={setTotalOrderPrice}/>
-      {totalOrderPrice > 0 && <BagCheckout sortedArr={sortedArr} totalOrderPrice={totalOrderPrice} />} */}
       {finalProductsArr.length > 0 && <BagList sortedArr={finalProductsArr} removeFromTheBag={removeFromTheBag} setFinalOrder={setFinalOrder} /> }
       {finalProductsArr.length > 0 && <BagCheckout sortedArr={finalProductsArr} /> }
     </div>
