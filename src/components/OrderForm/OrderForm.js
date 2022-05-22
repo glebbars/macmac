80da0809ca364b9a7ccd4fda3712f2ec767a273d
Gleb <glebars2004@gmail.com>
2022-05-22 14:42:33 +0100

    testing notification from github about bot: added bot

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -20,11 +20,11 @@ export const sendToTelegramBot = (data) => {
       return data
     }
 
-    // if(res.data.id > 0){
-    //   axios.post(`https://api.telegram.org/bot5001793522:AAGlAAbTMuMUUx-TqP_uWJfBf22nHw44Fys/sendMessage?chat_id=634614891&text=${dataToTelBot()}&parse_mode=html`)
-    //   .then(res => console.log(res))
-    //   .catch(err => console.log(err))
-    // }
+    if(res.data.id > 0){
+      axios.post(`https://api.telegram.org/bot5001793522:AAGlAAbTMuMUUx-TqP_uWJfBf22nHw44Fys/sendMessage?chat_id=634614891&text=${dataToTelBot()}&parse_mode=html`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+    }
   })
 }
 
