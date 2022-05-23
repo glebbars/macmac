dc363b2e657810160b7e245d9a21f933b302a1d0
Gleb <glebars2004@gmail.com>
2022-05-23 14:45:14 +0100

    implemented usd exchange rate into excel file

--- src/admin/UploadPrices/UploadPrices.js
+++ src/admin/UploadPrices/UploadPrices.js
@@ -11,22 +11,26 @@ const UploadPrices = () => {
 
   const productsArr = useSelector(store => store.app.productsArr);
 
-  const [usdExchangeRate, setUsdExchangeRate] = useState('')
+  // const [usdExchangeRate, setUsdExchangeRate] = useState(0)
 
 
-  useEffect(() => {
-    axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
-    .then(res => {
-      res.data.find(obj => obj.ccy === 'USD' && obj.base_ccy === 'UAH' ? setUsdExchangeRate(+obj.sale) : null)
-    })
-  }, [])
+  // useEffect(() => {
+  //   axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+  //   .then(res => {
+  //     res.data.find(obj => obj.ccy === 'USD' && obj.base_ccy === 'UAH' ? setUsdExchangeRate(+obj.sale) : null)
+  //   })
+  // }, [])
 
   const allPricesObj = {}
 
-  const updateObjPricesWithExcel = (e, usdExchangeRate) => {
+  const updateObjPricesWithExcel = (e) => {
   readXlsxFile(e.target.files[0], { sheet: 1 }).then( rows => {
+    const usdExchangeRate = rows[0][0] === 'Курс' ? rows[0][1] : 0
+
+    console.log(usdExchangeRate)
+
     rows.forEach(row => {
-      if(row[0] && row[1] && typeof row[1] === 'number'){
+      if(row[0] && row[1] && row[0] !== 'Курс' && typeof row[1] === 'number'){
         const formattedPrice = Math.round(Math.ceil(row[1] * usdExchangeRate)/5)*5
         const formattedNames = row[0].toLowerCase()
         allPricesObj[formattedNames] = formattedPrice
@@ -36,7 +40,7 @@ const UploadPrices = () => {
   })
   .then(data => axios.patch('http://localhost:5000/prices/1', data))
   .then(res => updateProductsPrices())
-  }
+}
   
   const updateProductsPrices = () => {
     if(productsArr.length > 0){
@@ -50,7 +54,7 @@ const UploadPrices = () => {
   return (
     <label className="admin__upload-file">
       {/* <div onClick={updateProductsPrices}>21</div> */}
-      <input className="admin__upload-file__input" type="file" onChange={e => updateObjPricesWithExcel(e, usdExchangeRate)}/>
+      <input className="admin__upload-file__input" type="file" onChange={e => updateObjPricesWithExcel(e)}/>
         <span className="admin__upload-file__text">Upload prices</span>
     </label>
   )
