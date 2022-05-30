afadf0bd7134e8f7a85503ccad320490c4195e4a
Gleb <glebars2004@gmail.com>
2022-05-31 00:36:58 +0100

    updated bestselers

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-const express = require('express');
-const jsonServer =  require('json-server');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({ static: './build' });
 const port = process.env.PORT || 5000;
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({ '/api/*': '/$1', })) 
 server.use(router);
 
-server.listen(port, () => { console.log('Server is running')})
+server.listen(port, () => { console.log('Server is running')});
