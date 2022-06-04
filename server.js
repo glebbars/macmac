7c0dfba883384e6e237f3e2fdab0f1e33a55a3d9
Gleb <glebars2004@gmail.com>
2022-06-04 23:08:12 +0100

    changed serve.js

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({ static: './build' });
 const port = process.env.PORT || 5000;
 
-app.use(middlewares);
-app.use(router);
+// app.use(middlewares);
+// app.use(jsonServer.rewriter({ '/api/*': '/$1', })) 
+// app.use(router);
 
 app.use('/db.json', middlewares, router);
-app.use(jsonServer.rewriter({ '/api/*': '/$1', })) 
 app.use(express.static(path.join(__dirname, 'build')));
-
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
