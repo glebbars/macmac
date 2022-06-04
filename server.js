13475c93b876928ba2a8824426fcd83f623bba89
Gleb <glebars2004@gmail.com>
2022-06-04 22:34:42 +0100

    changed serve.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(jsonServer.rewriter({ '/api/*': '/$1', }))
 app.use(router);
 
 // app.use('/db', middlewares, router);
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
