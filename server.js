dd968aae889bbd12667b0ee1018a985ff71a0a75
Gleb <glebars2004@gmail.com>
2022-06-04 23:35:59 +0100

    deleted static.json

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(router);
 // app.use('/db.json', middlewares, router);
 // app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/*', function (req, res) {
-//     console.log('***')
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', function (req, res) {
+    console.log('***')
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log('server is running'))
